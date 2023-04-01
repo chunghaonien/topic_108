547e7d67eb0fc48a607e6bf1cb66ae866188cdfd
chunghaonien <10856040@ntub.edu.tw>
2023-04-01 23:36:27 +0800

    4/2 copilot test

---
+++ Flask/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import mod.scraper
+from mod.into_mysql import into_mysql
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/start', methods=['POST'])
+def start():
+    url = request.form['url-input']
+    option = request.form.get('option', '')
+    save_data = request.form.get('save-data')
+    sql_date = ""
+
+    if url == "":
+        return render_template('index.html', date="請輸入網址")
+    else:
+        if option == 'html':
+            date = mod.scraper.html(url)
+        elif option == 'bs4':
+            date = mod.scraper.bs4(url)
+        elif option == '':
+            date = '請選擇解析方式'
+
+        if save_data:
+            sql_date = into_mysql(url, date)
+
+        return render_template('index.html', date=date, sql_date=sql_date)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)

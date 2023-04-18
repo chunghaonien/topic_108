a5e0f1a25d3aa45769350a3458e684be2d43e6b2
chunghaonien <10856040@ntub.edu.tw>
2023-04-18 20:46:08 +0800

    4/18 巴哈版面抓取測試完成

--- main/test.py
+++ main/test.py
@@ -1,2 +1,39 @@
-1231465234153
-7777777777777
+import urllib.request
+from lxml import etree
+from bs4 import BeautifulSoup
+
+url = 'https://forum.gamer.com.tw/B.php?page=1&bsn=36730'
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
+}
+
+request = urllib.request.Request(url = url, headers= headers)
+
+response = urllib.request.urlopen(request)
+content = response.read().decode('utf-8')
+
+tree = etree.HTML(content)
+url_list = tree.xpath("//td[@class='b-list__main']/a/@href")
+
+print(url_list)
+
+for url in url_list:
+   
+    url1 = 'https://forum.gamer.com.tw/' + str(url)
+    request = urllib.request.Request(url = url1, headers= headers)
+    response = urllib.request.urlopen(request)
+    content = response.read().decode('utf-8')
+    soup = BeautifulSoup(content, 'html.parser')
+    title_element = soup.find('h1', class_='c-post__header__title')
+    author_element = soup.find('div', class_='c-article__content')
+
+    if title_element:
+        title = title_element.text
+        print(title)
+    else:
+        print("未找到標題元素")
+    if author_element:
+        author = author_element.text
+        print(author)
+    else:
+        print("未找到內容元素")
--- ptt抓取/哈.py
+++ ptt抓取/哈.py
@@ -21,37 +21,43 @@ def get_content(request):
 
 def get_url(content):
     tree = etree.HTML(content)
-    url_list = tree.xpath("/html/body/div[@id='BH-background']/div[@id='BH-wrapper']/div[@id='BH-master']/form/div[@class='b-list-wrap']/table[@class='b-list']/tbody/tr[@class='b-list__row']/td[@class='b-list__main']//@href")
+    url_list = tree.xpath("//td[@class='b-list__main']/a/@href")
 
-    print(url_list)
     return url_list
 
-def get_article(url):
+
+def get_article(url_list):
     headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
     }
 
-    url1 = 'https://forum.gamer.com.tw/' + url
-    request = urllib.request.Request(url = url1, headers= headers)
-    response = urllib.request.urlopen(request)
-    content = response.read().decode('utf-8')
+    for url in url_list:
+        url1 = 'https://forum.gamer.com.tw/' + str(url)
+        request = urllib.request.Request(url = url1, headers= headers)
+        response = urllib.request.urlopen(request)
+        content = response.read().decode('utf-8')
+        soup = BeautifulSoup(content, 'html.parser')
+        title_element = soup.find('h1', class_='c-post__header__title')
+        author_element = soup.find('div', class_='c-article__content')
         
-    soup = bs4.BeautifulSoup(content, 'html.parser')
-    title = soup.find('h1', class_='c-post__header__title ').text
-    author = soup.find('div', class_='c-article__content').text
-    print(title)
-    print(author)
+        if title_element:
+            title = title_element.text
+            print(title)
+        else:
+            print("未找到標題元素")
+        if author_element:
+            author = author_element.text
+            print(author)
+        else:
+            print("未找到內容元素")
 
 
 
 if __name__ == '__main__':
     start_page = int(input('輸入起始頁碼'))
     end_page = int(input('輸入結束頁碼'))
-
     for page in range(start_page, end_page+1):
         request = create_request(page)
         content = get_content(request)
         url_list = get_url(content)
-
-        for url in url_list:
-            get_article(url)
+        get_article(url_list)

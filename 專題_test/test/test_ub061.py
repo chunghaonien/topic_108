9469879f8c2a74e18c9fef105b7f77a5aba83fb1
chunghaonien <10856040@ntub.edu.tw>
2023-05-30 23:31:40 +0800

    5/30 更新

--- 專題_test/test/test_ub061.py
+++ 專題_test/test/test_ub061.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-url = 'https://movie.douban.com/j/chart/top_list?type=5&interval_id=100%3A90&action=&start=0&limit=20'
+url = 'https://discord.com/channels/651047971214983189/651047971214983192/1112733174385082469'
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
@@ -14,5 +14,5 @@ content = response.read().decode('utf-8')
 #fp = open('douban.json', 'w', encoding='utf-8')
 #fp.write(content)
 
-with open('doban.json', 'w',encoding='utf-8') as fp:
+with open('123.html', 'w',encoding='utf-8') as fp:
     fp.write(content)

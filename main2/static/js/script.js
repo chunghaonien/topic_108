4e5eb6376455700c24908fbdc12dafee1e3245ec
chunghaonien <10856040@ntub.edu.tw>
2023-06-01 00:01:48 +0800

    Auto stash before merge of "main" and "origin/main"

--- main2/static/js/script.js
+++ main2/static/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 const body = document.querySelector('body');
 const sidebar = body.querySelector('.sidebar');
@@ -33,3 +34,40 @@ loginBtn.addEventListener('click', () => {
 body.addEventListener('click', () => {
     popupContainer.classList.remove('close');
 });
+=======
+
+const body = document.querySelector('body');
+const sidebar = body.querySelector('.sidebar');
+const toggle = body.querySelector('.toggle');
+const searchBtn = body.querySelector('.search-box');
+const modeSwitch = body.querySelector('.toggle-switch');
+const modeText = body.querySelector('.mode-text');
+const loginBtn = body.querySelector('.login');
+const popupContainer = body.querySelector('.popup-container');
+
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('close');
+});
+
+searchBtn.addEventListener('click', () => {
+    sidebar.classList.remove('close');
+});
+
+modeSwitch.addEventListener('click', () => {
+    body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+        modeText.textContent = '亮色模式';
+    } else {
+        modeText.textContent = '深色模式';
+    }
+});
+
+loginBtn.addEventListener('click', () => {
+    popupContainer.classList.toggle('close');
+});
+
+body.addEventListener('click', () => {
+    popupContainer.classList.remove('close');
+});
+>>>>>>> Stashed changes

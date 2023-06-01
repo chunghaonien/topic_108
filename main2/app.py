30c58ef0a417987d4904722224ad4bf94e347331
chunghaonien <10856040@ntub.edu.tw>
2023-06-01 19:30:17 +0800

    6/1 登入系統完成

--- main2/app.py
+++ main2/app.py
@@ -1,35 +1,45 @@
-from flask import Flask, render_template, request
-from mod.user import login, register
+from flask import Flask, render_template, request, jsonify
+from mod.user import login, register, get_user_data
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+user_logged_in = False
+global login_account
+global login_password
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login_route():
-    if request.method == "POST":
-        account = request.form.get('account')
-        password = request.form.get('password')
-        if login(account, password):
-            return render_template('ok.html')
-        else:
-            return render_template('no.html')
+    account = request.form.get('account')
+    password = request.form.get('password')
+    if login(account, password):
+        login_account = account
+        login_password = password
+        user = get_user_data(login_account, login_password)
 
-@app.route('/register', methods=['GET', 'POST'])
+        return render_template('index.html', user_logged_in=True, username=user[0]['username'], user_data=jsonify(user))
+    else:
+        return render_template('no.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
 def register_route():
-    if request.method == "POST":
-        account = request.form.get('account')
-        password = request.form.get('password')
-        username = request.form.get('username')
-        if register(account, password, username):
-            return render_template('ok.html')
-        else:
-            return render_template('no.html')
+
+    account = request.form['account']
+    password = request.form['password']
+    username = request.form['username']
+
+    if register(account, password, username):
+        return render_template('ok.html')
+    else:
+        return render_template('no.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
--- main2/mod/user.py
+++ main2/mod/user.py
@@ -3,11 +3,11 @@ import mysql.connector
 def login(account, password):
     try:
         db = mysql.connector.connect(
-        host="140.131.114.242",
-        port="3306",
-        user="admin112510",
-        password="@aA0937404883",
-        database="112-webpy"
+            host="140.131.114.242",
+            port="3306",
+            user="admin112510",
+            password="@aA0937404883",
+            database="112-webpy"
         )
 
         cursor = db.cursor()
@@ -20,28 +20,59 @@ def login(account, password):
             return True
         else:
             return False
-    except:
-        return False
+    finally:
+        db.close()
 
 def register(account, password, username):
     try:
         db = mysql.connector.connect(
-        host="140.131.114.242",
-        port="3306",
-        user="admin112510",
-        password="@aA0937404883",
-        database="112-webpy"
+            host="140.131.114.242",
+            port="3306",
+            user="admin112510",
+            password="@aA0937404883",
+            database="112-webpy"
         )
 
         cursor = db.cursor()
         sql = "INSERT INTO user (account, password, username) VALUES (%s, %s, %s)"
         val = (account, password, username)
-
         cursor.execute(sql, val)
         db.commit()
-        if cursor.rowcount == 1:
-            return True
-        else:
-            return False
+
+        
+        return True
     except:
+        
         return False
+
+def get_user_data(account, password):
+    try:
+        db = mysql.connector.connect(
+            host="140.131.114.242",
+            port="3306",
+            user="admin112510",
+            password="@aA0937404883",
+            database="112-webpy"
+        )
+
+        cursor = db.cursor()
+        sql = "SELECT * FROM user WHERE account = %s AND password = %s"
+        val = (account, password)
+        cursor.execute(sql, val)
+        result = cursor.fetchall()
+
+        user_data = []
+        for row in result:
+            user = {
+                'id': row[0],
+                'account': row[1],
+                'password': row[2],
+                'username': row[3],
+                'email': row[4],
+                'birthday': row[5]
+            }
+            user_data.append(user)
+        
+        return user_data
+    finally:
+        db.close()

39a60d977e6e82373a96efd75f8a73b3f851747f
學校筆電 <qqq0245@gmail.com>
2023-06-01 13:48:56 +0800

    6/1 登入介面

--- main2/static/js/script.js
+++ main2/static/js/script.js
@@ -5,8 +5,8 @@ const toggle = body.querySelector('.toggle');
 const searchBtn = body.querySelector('.search-box');
 const modeSwitch = body.querySelector('.toggle-switch');
 const modeText = body.querySelector('.mode-text');
-const loginBtn = body.querySelector('.login');
-const popupContainer = body.querySelector('.popup-container');
+const loginBtn = body.querySelector('.l');
+const loginWindow = body.querySelector('.login-window');
 
 toggle.addEventListener('click', () => {
     sidebar.classList.toggle('close');
@@ -24,12 +24,4 @@ modeSwitch.addEventListener('click', () => {
     } else {
         modeText.textContent = '深色模式';
     }
-});
-
-loginBtn.addEventListener('click', () => {
-    popupContainer.classList.toggle('close');
-});
-
-body.addEventListener('click', () => {
-    popupContainer.classList.remove('close');
 });

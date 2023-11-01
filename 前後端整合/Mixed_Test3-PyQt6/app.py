fc93125681b9f8cd0a53c00fff412a4f94b9411e
chunghaonien <10856040@ntub.edu.tw>
2023-11-01 17:14:00 +0800

    11/1 更新PyQt6

---
+++ 前後端整合/Mixed_Test3-PyQt6/app.py
@@ -0,0 +1,10 @@
+from package import *
+import time
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    web_browser_window = WebBrowserWindow(main_window)
+    web_browser_window.scraping_button.clicked.connect(web_browser_window.scrape_data)
+    
+    app.exec()

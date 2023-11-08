dae39a800ea677d5c51b354106f9b6df4e03c4e2
chunghaonien <10856040@ntub.edu.tw>
2023-11-08 22:23:29 +0800

    11/8 伺服器

--- main2/mod/user.py
+++ main2/mod/user.py
@@ -1,78 +1,39 @@
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel, QStackedWidget, QMessageBox
 import mysql.connector
 
-def login(account, password):
-    try:
-        db = mysql.connector.connect(
-            host="140.131.114.242",
-            port="3306",
-            user="admin112510",
-            password="@aA0937404883",
-            database="112-webpy"
-        )
+# ...（之前的代码）
 
-        cursor = db.cursor()
-        sql = "SELECT * FROM user WHERE account = %s AND password = %s"
-        val = (account, password)
+class MyApp(QApplication):
+    def __init__(self, sys_argv):
+        super().__init__(sys_argv)
+        
+        self.setStyle("fusion")  # 设置应用程序的样式为"fusion"
+        
+        self.stack = QStackedWidget()
+        self.login_widget = LoginWidget()
+        self.main_window = MainWindow()
+        self.stack.addWidget(self.login_widget)
+        self.stack.addWidget(self.main_window)
+        
+        self.stack.setCurrentWidget(self.login_widget)
+        
+        self.login_widget.login_button.clicked.connect(self.switch_to_main_window)
 
-        cursor.execute(sql, val)
-        result = cursor.fetchall()
-        if result:
-            return True
+    def switch_to_main_window(self):
+        # 获取输入的用户名和密码
+        username = self.login_widget.username_input.text()
+        password = self.login_widget.password_input.text()
+        
+        # 调用登录验证函数
+        if login(username, password):
+            self.stack.setCurrentWidget(self.main_window)
+            self.main_window.show()  # 显示主窗口
         else:
-            return False
-    finally:
-        db.close()
+            QMessageBox.critical(self.main_window, "Login Failed", "Invalid username or password. Please try again.")
 
-def register(account, password, username):
-    try:
-        db = mysql.connector.connect(
-            host="140.131.114.242",
-            port="3306",
-            user="admin112510",
-            password="@aA0937404883",
-            database="112-webpy"
-        )
-
-        cursor = db.cursor()
-        sql = "INSERT INTO user (account, password, username) VALUES (%s, %s, %s)"
-        val = (account, password, username)
-        cursor.execute(sql, val)
-        db.commit()
-
-        
-        return True
-    except:
-        
-        return False
-
-def get_user_data(account, password):
-    try:
-        db = mysql.connector.connect(
-            host="140.131.114.242",
-            port="3306",
-            user="admin112510",
-            password="@aA0937404883",
-            database="112-webpy"
-        )
-
-        cursor = db.cursor()
-        sql = "SELECT * FROM user WHERE account = %s AND password = %s"
-        val = (account, password)
-        cursor.execute(sql, val)
-        result = cursor.fetchall()
-
-        user_data = []
-        for row in result:
-            user = {
-                'id': row[0],
-                'account': row[1],
-                'password': row[2],
-                'username': row[3],
-                'email': row[4],
-                'birthday': row[5]
-            }
-            user_data.append(user)
-        
-        return user_data
-    finally:
-        db.close()
+if __name__ == "__main__":
+    app = MyApp(sys.argv)
+    app.exec()
---
+++ 遠端/test2.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+async def send_data():
+    uri = "ws://26.30.97.239:8765"  # 請替換為虛擬機的 IP 地址和端口
+
+    async with websockets.connect(uri) as websocket:
+        while True:
+            data = input("輸入要發送的數據: ")
+            await websocket.send(data)  # 向服務端發送數據
+
+            response = await websocket.recv()  # 接收服務端的回覆
+            print(f"Received response: {response}")
+
+asyncio.get_event_loop().run_until_complete(send_data())

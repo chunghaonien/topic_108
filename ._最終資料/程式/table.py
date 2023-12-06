e479420050edeac79048b9600c861d98dc7d6174
10856031 <10856031@ntub.edu.tw>
2023-12-06 21:00:06 +0800

    文字篩選結束、沒資料無法查詢會跳視窗

--- ._最終資料/程式/table.py
+++ ._最終資料/程式/table.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QLabel, QHeaderView, QInputDialog, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QLabel, QHeaderView, QInputDialog, QMessageBox, QLineEdit
 from PyQt6.QtCore import Qt, QAbstractTableModel, QVariant
 import sys
 import subprocess
@@ -78,6 +78,16 @@ class MainWindow(QMainWindow):
         self.select_button.setStyleSheet("background-color: #008CBA;")
         self.select_button.clicked.connect(self.select_button_clicked)
 
+# ////////////////////////////////
+        self.filter_button = QPushButton("篩選")
+        self.filter_label = QLabel('輸入篩選值', self)
+        self.filter_textbox = QLineEdit(self)
+        self.filter_button.setFixedSize(80, 30)
+        self.filter_button.setStyleSheet("background-color: #008CBA;")
+        self.filter_button.clicked.connect(self.filter_button_clicked)
+        self.filter_button.setEnabled(False)  # 初始狀態設為不可用
+        self.filter_button.setStyleSheet("background-color: #CCCCCC; color: #555555;")  # 灰色樣式
+# ////////////////////////////////
         # 新增下載按鈕
         self.download_button = QPushButton("下載")
         self.download_button.setFixedSize(80, 30)
@@ -88,19 +98,33 @@ class MainWindow(QMainWindow):
         account_label = QLabel(f'用戶名 : {username}', self)
         user_id_label = QLabel(f'用戶id : {user_id}', self)
 
+        # #設置一個選擇時間的下拉式選單按鈕
+        # self.time_button = QPushButton('選擇時間', self)
+        # self.time_button.setStyleSheet("background-color: #008CBA;")
+        # self.time_button.setFixedSize(80, 30)
+        # self.time_button.clicked.connect(self.filter_by_time)
+
         # 將按鈕添加到水平佈局
         h_layout = QHBoxLayout()
         h_layout.addWidget(account_label)
         h_layout.addWidget(user_id_label)
         h_layout.addStretch(1)  #將按鈕推到右邊
+        # h_layout.addWidget(self.time_button)
+        h_layout.addWidget(self.filter_label)
+        h_layout.addWidget(self.filter_textbox)
+        h_layout.addWidget(self.filter_button)
         h_layout.addWidget(self.select_button)
         h_layout.addWidget(self.download_button)
+        
 
         # 添加水平佈局到垂直佈局
         v_layout.addLayout(h_layout)
 
         layout.addLayout(v_layout)
         self.setCentralWidget(central_widget)
+
+
+
 
     def download_button_clicked(self):
         global rows
@@ -144,7 +168,7 @@ class MainWindow(QMainWindow):
             print(f"下載資料時發生錯誤: {e}")
 
 
-    def select_button_clicked(self, download_button):
+    def select_button_clicked(self):
         global rows
 
         response = asyncio.run(main_async(self.user_id))
@@ -159,18 +183,46 @@ class MainWindow(QMainWindow):
 
         # 將 response 中的 datetime.date 對象替换字符
         response_with_dates_as_strings = date_pattern.sub(date_replacer, response)
+        print(response_with_dates_as_strings)
+    
+        if response_with_dates_as_strings == "False":
+            QMessageBox.warning(self, "警告", "沒有資料可顯示。")
+            return
+        else:
+            try:
+                # 使用 ast.literal_eval 將修改的字串轉換列表
+                rows_temp = ast.literal_eval(response_with_dates_as_strings)
 
-        try:
-            # 使用 ast.literal_eval 將修改的字串轉换列表
-            rows_temp = ast.literal_eval(response_with_dates_as_strings)
+                # 將日期字符串轉換回 datetime.date 對象
+                rows = [(user_id, datetime.datetime.strptime(date_string, "%Y-%m-%d").date(), data)
+                        for user_id, date_string, data in rows_temp]
+                
 
-            # 將日期字符串轉换回 datetime.date 對象
-            rows = [(user_id, datetime.datetime.strptime(date_string, "%Y-%m-%d").date(), data)
-                    for user_id, date_string, data in rows_temp]
+            except Exception as e:
+                print(f"An error occurred while parsing the response: {e}")
+                rows = []
 
-        except Exception as e:
-            print(f"An error occurred while parsing the response: {e}")
-            rows = []
+        self.model.layoutAboutToBeChanged.emit()
+        self.model.layoutChanged.emit()
+
+        self.download_button.setEnabled(True)
+        self.download_button.setStyleSheet("")
+        # 在查詢按鈕觸發後，將篩選按鈕設置為可用
+        self.filter_button.setEnabled(True)     #資料傳送
+        self.filter_button.setStyleSheet("")    # 移除樣式，恢復預設外觀
+
+    def filter_button_clicked(self):
+        global rows
+
+        # 取得篩選值
+        filter_value = self.filter_textbox.text()
+
+        if not filter_value:
+            QMessageBox.warning(self, "警告", "請輸入篩選值。")
+            return
+
+        # 使用 filter() 函數篩選資料
+        rows = list(filter(lambda row: filter_value in row[2], rows))
 
         self.model.layoutAboutToBeChanged.emit()
         self.model.layoutChanged.emit()
@@ -178,11 +230,23 @@ class MainWindow(QMainWindow):
         self.download_button.setEnabled(True)
         self.download_button.setStyleSheet("")
 
+    # # 依照時間進行篩選
+    # def filter_by_time(self, time):
+    #     global rows
+
+    #     # 使用 filter() 函數篩選資料
+    #     rows = list(filter(lambda row: time in row[1], rows))
+
+    #     self.model.layoutAboutToBeChanged.emit()
+    #     self.model.layoutChanged.emit()
+
+    #     self.download_button.setEnabled(True)
+    #     self.download_button.setStyleSheet("")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    user_data = sys.stdin.read().strip()
-    # user_data = "test,1"
+    # user_data = sys.stdin.read().strip()
+    user_data = "test,1"
     username = user_data.split(",")[0]
     user_id = user_data.split(",")[1]
 
